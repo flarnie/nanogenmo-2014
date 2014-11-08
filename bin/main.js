4618192db2e73c5e6166be420b5a923d6ef36193
Flarnie Marchan <fnonemaker@gmail.com>
2014-11-08 10:30:45 -0800

    Add initial markov chain generator and fix bugs
    This adds the initial version of the fiction generator, using a very
    basic markov chain:
    1. Find a sentence begining with your starter word. This is your 'found
    sentence'.
    2. Reset the 'starter word' to the word that began the sentence
    following your 'found sentence'.
    3. If at any point you run out of starter words or sentences that start
    with those words, choose a random starter word from the possible starter
    words.
    
    To make this work, I lowercase all starter words when saving them with
    the parser.
    
    A test in main.js prints out a short piece of generated fiction.

--- bin/main.js
+++ bin/main.js
@@ -6,8 +6,8 @@ textParser(seedTextFilename);
 
 // 2. Report on the contents of the JSON file, and inspect it to make sure it
 // looks good.
+/*
 var fs = require('fs');
-
 var jsonFilename = 'parsed_text/pirates_1.json';
 var pirateSentences = JSON.parse(fs.readFileSync(jsonFilename));
 for (var starterWord in pirateSentences) {
@@ -16,9 +16,18 @@ for (var starterWord in pirateSentences) {
     console.log('The starter word ', starterWord, ' begins ', count, ' sentences.');
   }
 }
+*/
 
 // 3. Use the JSON data to generate a markdown file with a paragraph of text,
 // using a markov chain.
+var generateFiction = require('../lib/generate_fiction');
+generateFiction({
+  jsonFilename: 'pirates_1',
+  pageTitle: 'Test: Pirates',
+  minimumWords: 500,
+  startsWith: 'piracy'
+});
+
 // 4. Look at the generated text, and make sure it looks ok.
 // 5. Generate the entire 5 chapters of the novel.
 // 6. Use grunt-markdown-pdf to turn the markdown pages into a pdf book.
