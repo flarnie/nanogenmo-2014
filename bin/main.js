0d00713c9534309ff0993a9a090253592f1af55b
Flarnie Marchan <fnonemaker@gmail.com>
2014-11-08 09:06:54 -0800

    Add main.js and text parser module
    The text parser generates an index of sentences categorized by the first
    word which they start with.
    
    I could have used 'split' to handle the initial seed text, but I prefer
    to step through it one character at a time and parse it that way. This
    is lighter on memory usage, and still performs in about linnear
    complexity. It also gives some flexibility in parsing different types of
    things - for example, I change newlines into spaces as I go. That
    would be possible if using 'split', but it would work slightly
    differently - it might take an extra step.

---
+++ bin/main.js
@@ -0,0 +1,29 @@
+// The Plan:
+// 1. Analyze a seed text and turn it into a JSON file
+var textParser = require('../lib/text_parser');
+var seedTextFilename = 'pirates_1';
+textParser(seedTextFilename);
+
+// 2. Report on the contents of the JSON file, and inspect it to make sure it
+// looks good.
+//var fs = require('fs');
+
+//var jsonFilename = 'parsed_text/pirates_1.json';
+//var pirateSentences = JSON.parse(fs.readFileSync(jsonFilename));
+/*
+for (var starterWord in pirateSentences) {
+  if (pirateSentences.hasOwnProperty(starterWord)) {
+    var count = pirateSentences[starterWord].length;
+    console.log('The starter word ', starterWord, ' begins ', count, ' sentences.');
+  }
+}
+*/
+
+// 3. Use the JSON data to generate a markdown file with a paragraph of text,
+// using a markov chain.
+// 4. Look at the generated text, and make sure it looks ok.
+// 5. Generate the entire 5 chapters of the novel.
+// 6. Use grunt-markdown-pdf to turn the markdown pages into a pdf book.
+// https://www.npmjs.org/package/grunt-markdown-pdf
+//
+// Victory!
